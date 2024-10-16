a4ec1f648f5750a437cc6e38f10198eebe5ec8ec
JuanCarrillo2435 <juancarrillo.dc@gmail.com>
2024-10-15 22:12:37 -0300

    login view

--- app.js
+++ app.js
@@ -24,6 +24,14 @@ app.get('/',(req,res)=>{
   res.render('home');
 })
 
+app.get('/auth/register', (req, res) => {
+  res.render('login'); 
+});
+
+app.get('/auth/login', (req, res) => {
+  res.render('login'); 
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
