bceb985db1040fa3c761b2bd1c10f7962f12c5aa
Lucero Roque Roberto Miguel <roquerobertomiguellucero@gmail.com>
2024-10-29 10:17:10 -0300

    pequeña identación

--- services/profesionalService.js
+++ services/profesionalService.js
@@ -1,10 +1,10 @@
 const Profesional = require('../models/profesionalModel');
-const ProfesionalEspecialidad = require ('../models/profesionalEspecialidadModel');
+const ProfesionalEspecialidad = require('../models/profesionalEspecialidadModel');
 
 const profesionalService = {
   crearProfesional: (nombre_completo) => {
     return new Promise((resolve, reject) => {
-      Profesional.crear(nombre_completo,(err, result) => {
+      Profesional.crear(nombre_completo, (err, result) => {
         if (err) {
           return reject(err); //rechazamos la promesa
         }
@@ -22,35 +22,35 @@ const profesionalService = {
     });
   },
 
-  crearProfesionalEspecialidad : async (nombre_completo, especialidad_id, matricula) => {
+  crearProfesionalEspecialidad: async (nombre_completo, especialidad_id, matricula) => {
     try {
       console.log("🚀 ~ Crear Profesional ~ nombre_completo, especialidad_id, matricula:", nombre_completo, especialidad_id, matricula);
-      
+
       const result = await new Promise((resolve, reject) => {
         Profesional.crear(nombre_completo, especialidad_id, matricula, (err, res) => {
           if (err) return reject(err);
           resolve(res);
         });
       });
-  
+
       console.log("🚀 ~ Profesional creado ~ result:", result);
-  
+
       const resultPE = await new Promise((resolve, reject) => {
         ProfesionalEspecialidad.crear(result.profesional_id, especialidad_id, matricula, (errPE, resPE) => {
           if (errPE) return reject(errPE);
           resolve(resPE);
         });
       });
-  
+
       console.log("🚀 ~ ProfesionalEspecialidad creado ~ resultPE:", resultPE);
       return resultPE;
-      
+
     } catch (error) {
       console.error("Error en crearProfesionalEspecialidad:", error);
       throw error;
     }
   }
-  
+
 };
 
 module.exports = profesionalService;
