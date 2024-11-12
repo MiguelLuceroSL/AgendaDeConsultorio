e261c1788abae37749be9e32085bae2109fa8329
Lucero Roque Roberto Miguel <roquerobertomiguellucero@gmail.com>
2024-11-12 16:19:37 -0300

    config .env
    configuracion del .env para la db para ya hacer el deploy en render

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get('/crearpaciente', (req, res) => {
   res.render('crearPaciente');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
 import mysql from 'mysql2';
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'agenda_consultorio'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
