baaaa88f538c667d073e579d429b6c1b0c67a1ea
JuanCarrillo2435 <juancarrillo.dc@gmail.com>
2025-12-18 10:17:42 -0300

    lista de medicos mejorada

--- public/js/profesionales.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('select[name="especialidad"]').addEventListener('change', function () {
-  const especialidad = this.value;
-  fetch(`/profesional/listar?especialidad=${especialidad}`)
-    .then(response => response.text())
-    .then(html => {
-      document.body.innerHTML = html;
-    })
-    .catch(error => console.error('Error al cargar los profesionales:', error));
-});
